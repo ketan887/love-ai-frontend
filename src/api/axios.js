c41a4d446c9536ae3639a9a7a775630138a20114
ketan887 <lokhandeketan427@gmail.com>
2025-09-25 11:41:28 +0530

    fixed chat page

--- src/api/axios.js
+++ src/api/axios.js
@@ -22,11 +22,31 @@
 
 
 
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", 
+//   withCredentials: true,
+// });
+
+// // ✅ Attach JWT token automatically if present
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default API;
+
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", 
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL, // automatically picks local or production
+  withCredentials: true, // sends cookies / JWT if needed
 });
 
 // ✅ Attach JWT token automatically if present
